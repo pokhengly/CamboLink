90f9d1f8e460849b719997f46f1711f68251dc45
pokhengly <henglypok21012021@gmail.com>
2024-08-21 19:45:01 +0700

    Initial Commit

---
+++ CamboLink/CamboLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamboLinkApp.swift
+//  CamboLink
+//
+//  Created by KAK-LY on 21/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CamboLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamboLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CamboLink
+//
+//  Created by KAK-LY on 21/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
